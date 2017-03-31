1b734e782b319468a86638e4b23f7bfdbf8cf83f
Sean Lennaerts <seanlenny@gmail.com>
2017-03-30 21:57:49 -0700

    trying to make changes to .htaccess

--- src/Routes.php
+++ src/Routes.php
@@ -5,8 +5,8 @@ return [
     ['GET', '/', ['GTheatre\Controllers\Homepage', 'show']],
     ['POST', '/signout', ['GTheatre\Controllers\Homepage', 'signout']],
 // login page
-    ['GET', '/login', ['\..\src\Controllers\LoginPage', 'show']],
-    ['POST', '/login', ['\..\src\Controllers\LoginPage', 'login']],
+    ['GET', '/login', ['\GTheatre\Controllers\LoginPage', 'show']],
+    ['POST', '/login', ['\GTheatre\Controllers\LoginPage', 'login']],
     ['POST', '/account/customer/createCustomerAccount', ['GTheatre\Controllers\LoginPage', 'createCustomerAccount']],
     ['POST', '/account/employee/createEmployeeAccount', ['GTheatre\Controllers\LoginPage', 'createEmployeeAccount']],
 // account page
